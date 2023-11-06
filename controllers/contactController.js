f6590f92fc5ad252a016003537b5701a5218872e
SwapnilV26 <swapnilvish2812@gmail.com>
2023-11-06 23:14:08 +0530

    implemented error handling with middleware

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -1,38 +1,45 @@
+const asyncHandler = require("express-async-handler")
+
 //@desc Get all contacts
 //@route GET /api/contacts
 //@access public
-const getAllContacts = (req, res) => {
+const getAllContacts = asyncHandler(async (req, res) => {
   res.status(200).send("Get all contacts");
-};
+});
 
 //@desc Get particular contact
 //@route GET /api/contacts
 //@access public
-const getContact = (req, res) => {
+const getContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Get contact for ${req.params.id}` });
-};
+});
 
 //@desc Create new contact
 //@route POST /api/contacts
 //@access public
-const createContact = (req, res) => {
-    console.log(req.body)
+const createContact = asyncHandler(async (req, res) => {
+  console.log(req.body);
+  const {name, email, phone} = req.body;
+  if(!name || !email || !phone){
+    res.status(400);
+    throw new Error("All fields are mandatory !!!");
+  }
   res.status(201).send("Create contact");
-};
+});
 
 //@desc Update contact
 //@route PUT /api/contacts
 //@access public
-const updateContact = (req, res) => {
+const updateContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Update contact for ${req.params.id}` });
-};
+});
 
 //@desc Delete new contact
 //@route DELETE /api/contacts
 //@access public
-const deleteContact = (req, res) => {
+const deleteContact = asyncHandler(async (req, res) => {
   res.status(200).json({ message: `Delete contact for ${req.params.id}` });
-};
+});
 
 module.exports = {
   getAllContacts,
--- server.js
+++ server.js
@@ -2,10 +2,13 @@ const express = require('express');
 const app = express();
 
 const dotenv = require("dotenv").config();
+const errorHandler = require('./middleware/errorHandler');
+
 const port = process.env.PORT || 5000;
 
 app.use(express.json());
 app.use("/api/contacts", require("./routes/contactRoute"));
+app.use(errorHandler);
 
 app.listen(port, ()=>{
     console.log(`Server running on port ${port}`);
